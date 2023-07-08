daf9e03d7d10062e11d303d4e490c1524463595e
VincentBraunsfeld <vincent.braunsfeld7@gmail.com>
2023-07-08 20:13:08 +0200

    add fib final solution

--- tasks/JS/4_1.js
+++ tasks/JS/4_1.js
@@ -5,12 +5,12 @@ const button_identity = document.getElementById('btn1');
 const span_identity = document.createElement('span');
 button_identity.addEventListener('click', () => {
     let result = identity(input_identity.value);
-    span_identity.textContent = `${result}`;
+    span_identity.textContent = ` = ${result}`;
     p_identity.innerHTML += span_identity.outerHTML;
     input_identity.value = '';
 });
 input_identity.addEventListener('input', function( event ){
-    p_identity.textContent = 'identity( '+this.value+' ) = ';
+    p_identity.textContent = 'identity( '+this.value+' )';
 });
 
 function identity( input ){
@@ -22,11 +22,13 @@ const id_input = document.getElementById('id_function_input');
 const id_button = document.getElementById('id_function_btn');
 const id_span = document.getElementById('id_span');
 const p_test = document.createElement('p');
+p_test.classList.add('result');
 
 id_button.addEventListener('click', () => {
     const x = identity_function(id_input.value);
     const button_execute = document.createElement('button');
     button_execute.textContent = 'ausführen';
+    button_execute.classList.add('custom-button');
 
     button_execute.addEventListener('click', () => {
         p_test.innerHTML = `${x()}`;
@@ -47,23 +49,23 @@ function identity_function(param) {
 const add_input1 = document.getElementById('add_input1');
 const add_input2 = document.getElementById('add_input2');
 const add_button = document.getElementById('addBtn');
-const add_span = document.getElementById('add_span');
+const add_p = document.getElementById('add_p');
 
 const mul_input1 = document.getElementById('mul_input1');
 const mul_input2 = document.getElementById('mul_input2');
 const mul_button = document.getElementById('mulBtn');
-const mul_span = document.getElementById('mul_span');
+const mul_p = document.getElementById('mul_p');
 
 add_button.addEventListener('click', () => {
 
     let result = add(parseInt(add_input1.value), parseInt(add_input2.value));
     console.log(result);
-    add_span.textContent = `${result}`;
+    add_p.textContent = `${result}`;
 })
 
 mul_button.addEventListener('click', () => {
     let result = mul(parseInt(mul_input1.value), parseInt(mul_input2.value));
-    mul_span.textContent = `${result}`;
+    mul_p.textContent = `${result}`;
 })
 
 
@@ -80,12 +82,16 @@ const addf_input = document.getElementById('addf_function_input');
 const addf_button = document.getElementById('addf_function_btn');
 const addf_span = document.getElementById('addf_span');
 const addf_p = document.createElement('p');
+addf_p.classList.add('result');
 const addf_input2 = document.createElement('input');
+addf_input2.setAttribute('type', 'number');
+addf_input2.classList.add('custom-input')
 
 addf_button.addEventListener('click', () => {
     const x = addf(parseInt(addf_input.value));
     const button_execute = document.createElement('button');
     button_execute.textContent = 'ausführen';
+    button_execute.classList.add('custom-button')
 
     button_execute.addEventListener('click', () => {
         addf_p.innerHTML = `${x(parseInt(addf_input2.value))}`;
@@ -109,6 +115,7 @@ const input2_applyf = document.getElementById('input2_applyf');
 const button_applyf = document.getElementById('btn_applyf');
 const span_applyf = document.getElementById('span_applyf');
 const p_applyf = document.createElement('p');
+p_applyf.classList.add('result');
 
 button_applyf.addEventListener('click', () => {
     let bin_f = select_applyf.value === 'add' ? add : mul;
--- tasks/JS/4_3.js
+++ tasks/JS/4_3.js
@@ -2,54 +2,90 @@ const fib_button = document.getElementById('fib_button');
 const fib_input = document.getElementById('fib_input');
 const fib_table = document.getElementById('fib_table');
 const fib_span = document.getElementById('fib_span');
-const clear_button = document.createElement('button')
+const clear_button = document.createElement('button');
+const errorMessage = document.getElementById('error_message');
 clear_button.textContent = 'löschen';
-let clear_button_created = false;
+clear_button.classList.add('del-button');
+fib_span.appendChild(clear_button);
+let table_created = true;
+
+let fib_row = { 0: 1, 1: 1 };
+
 
 clear_button.addEventListener('click', () => {
     const rowCount = fib_table.rows.length;
-    for (let i = rowCount - 1; i >= 3; i--) {
+    fib_row = { 0: 1, 1:1};
+    errorMessage.textContent = '';
+    table_created = true;
+    for (let i = rowCount - 1; i >= 1; i--) {
         fib_table.deleteRow(i);
     }
 })
 
-let fib_row = { 0: 0, 1: 1 };
+
 
 fib_button.addEventListener('click', () => {
-    if(!clear_button_created){
-        fib_span.appendChild(clear_button);
-        clear_button_created = true;
-    }
+
+    errorMessage.textContent = '';
     const number = parseInt(fib_input.value);
+
+    if (number < 0) {
+        const errorMessage = document.getElementById('error_message');
+        errorMessage.textContent = 'Die eingegebene Zahl muss größer oder gleich Null sein.';
+        return;
+    }
+
+    if(table_created){
+        table_created = false;
+        if(number === 0){
+            addTableRow(0, 1, 0);
+        }
+        else {
+            addTableRow(0,1,0);
+            addTableRow(1, 1, 1);
+        }
+    }
+
+    resetColor();
+
     const keys = Object.keys(fib_row);
     const lastKey = parseInt(keys[keys.length - 1]);
-    fib(number);
-    const entries = Object.entries(fib_row);
 
-    for (let i = lastKey + 1; i <= number; i++) {
-        const [key, value] = entries[i];
-        addTableRow(key, value, number);
+    if(fib(number)) {
+        const entries = Object.entries(fib_row);
+        console.log(lastKey, number);
+        for (let i = lastKey + 1; i <= number; i++) {
+            const [key, value] = entries[i];
+            addTableRow(key, value, i);
+        }
     }
+    setColor(number)
 });
 
-function addTableRow(key, value, number) {
+function addTableRow(key, value, rowValue) {
     const tr = document.createElement('tr');
     const td1 = document.createElement('td');
     const td2 = document.createElement('td');
+    tr.setAttribute('data-index', rowValue);
     td1.textContent = key;
     td2.textContent = value;
-
-    if (key === number.toString()) {
-
-        tr.style.backgroundColor = ('green');
-    }
 
     tr.appendChild(td1);
     tr.appendChild(td2);
     fib_table.appendChild(tr);
 }
 
-function resetTable() {
+function setColor(number){
+    const rows = fib_table.querySelectorAll('tr');
+    rows.forEach(row => {
+        const dataIndex = row.getAttribute('data-index');
+        if (dataIndex === number.toString()) {
+            row.style.backgroundColor = '#129385';
+        }
+    });
+}
+
+function resetColor() {
     const rows = fib_table.querySelectorAll('tr');
     rows.forEach(row => {
         row.style.background = 'white';
@@ -57,7 +93,7 @@ function resetTable() {
 }
 
 function fib(x) {
-    if (fib_row.hasOwnProperty(x)) return;
+    if (fib_row.hasOwnProperty(x)) return false;
 
     let key = parseInt(Object.keys(fib_row)[Object.keys(fib_row).length - 1]);
     let n1 = BigInt(Object.values(fib_row)[Object.values(fib_row).length - 1]); // n-1
@@ -70,5 +106,5 @@ function fib(x) {
         n2 = temp;
         fib_row[key++] = n1;
     }
-    return n1;
+    return true;
 }
